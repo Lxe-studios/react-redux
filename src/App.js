f65febc874d157350f9325e4afeddb12bb6806a4
李雪儿 <>
2023-03-06 21:42:28 +0800

    3.6提交

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ import React from 'react';
 
 
 
+
 function App(props) {
-  const [count,setCount] = useState(0)
-  const clickCallback = useCallback(() => {
-    setCount(count + 1)
-  },[count])
+  
 
   return (
     <div>
-      <div>{count}</div>
-       <button onClick={() => {setCount(count + 1)}}>button</button>
-       <button onClick={clickCallback}>button2</button>
+
     </div>
   )
   
