08cc8d0dfdaa3da273507aea42de9dd28726e0e6
李雪儿 <>
2023-03-07 20:09:22 +0800

    3.7提交1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useCallback, useState } from 'react';
 import './App.css';
 import React from 'react';
 
-
+// useRef
+// 获取children 子节点 字组件
+// 类似于class 组件属性，组件的全局变量
 
 
 
@@ -16,9 +18,6 @@ function App(props) {
   )
   
 }
-
-
-
 
 
 export default App;
---
+++ src/hooks/useReducer.js
@@ -0,0 +1,24 @@
+import { useCallback, useState } from 'react';
+import './App.css';
+import React from 'react';
+
+// useReducer
+//useState 升级：复杂 包含多个子值 或 state依赖
+// const [state,dispatch] = useReducer(reducer,initialArg,init)
+// 参数： reducer ： （state，action） => newState 
+
+
+
+function App(props) {
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+  
+}
+
+
+export default App;
